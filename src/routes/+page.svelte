0c9af1cf1a168cb242a825a6f486fe018f30a7d2
ergitbeqiri <ergbeq21@htl-shkoder.com>
2025-07-08 09:57:35 +0100

    big changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+
+</script>
+
+<section class="flex items-center justify-center mt-10">
+    <h1 class="text-4xl font-bold">Test Pilot</h1>
+</section>
